77b0de6b7870afddfd37ee8994783af2a9c35aed
kaisei-tanita05 <k024g0081@g.neec.ac.jp>
2026-07-24 10:24:50 +0900

    04_02までやっちゃった

--- main.cpp
+++ main.cpp
@@ -1,8 +1,63 @@
 #include <Windows.h>
 #include <KamataEngine.h>
+#include "TitleScene.h"
 #include "GameScene.h"
 
 using namespace KamataEngine;
+
+TitleScene* titleScene = nullptr;
+GameScene* gameScene = nullptr;
+
+enum class Scene
+{ 
+	kUnknown,
+	kTitle,
+	kGame,
+};
+
+Scene scene = Scene::kUnknown;
+
+void ChangeScene()
+{ 
+	switch (scene) {
+	case Scene::kTitle:
+		gameScene->Initialize();
+		break;
+	case Scene::kGame:
+		//gameScene->UpDate();
+		break;
+	}
+}
+
+
+void UpdateScene() {
+	switch (scene) {
+	case Scene::kTitle:
+
+		titleScene->UpDate();
+
+		if (Input::GetInstance()->TriggerKey(DIK_RETURN)) {
+			scene = Scene::kGame;
+		}
+
+		break;
+
+	case Scene::kGame:
+		gameScene->UpDate();
+		break;
+	}
+}
+
+void DrawScene() {
+	switch (scene) {
+	case Scene::kTitle:
+		titleScene->Draw();
+		break;
+	case Scene::kGame:
+		gameScene->Draw();
+		break;
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -10,7 +65,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// エンジンの初期化
 	Initialize(L"LE3D_17_タニタ_カイセイ_CG4");
 
-	GameScene* gameScene = new GameScene();
+	scene = Scene::kTitle;
+
+	titleScene = new TitleScene();
+	titleScene->Initialize();
+	gameScene = new GameScene();
 	gameScene->Initialize();
 
 	// DirectXCommonインスタンスの取得
@@ -23,16 +82,23 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			break;
 		}
 
-		gameScene->UpDate();
+		// シーン切り替え
+		ChangeScene();
+
+		// 現在シーン更新
+		UpdateScene();
 
 		// 描画開始
 		dxCommon->PreDraw();
 
-		gameScene->Draw();
+		DrawScene();
 
 		// 描画終了
 		dxCommon->PostDraw();
 	}
+
+	titleScene->~TitleScene();
+	delete gameScene;
 
 	// エンジンの終了処理
 	Finalize();
